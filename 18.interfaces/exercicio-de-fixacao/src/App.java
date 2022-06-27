880858fdf50376534d35bb1d0ec30790ff46455c
arturrpereira <artur.fernando@protonmail.com>
2022-06-26 23:55:55 -0300

    resolução do exercício de fixação sobre interfaces

--- 18.interfaces/exercicio-de-fixacao/src/App.java
+++ 18.interfaces/exercicio-de-fixacao/src/App.java
@@ -1,7 +1,40 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Contract;
+import model.service.ContractService;
+import model.service.PaypalService;
+import model.entities.Installment;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter contract data");
+        System.out.print("Number: ");
+        Integer number = sc.nextInt();
+        System.out.print("Date (dd/MM/yyyy):");
+        Date date = sdf.parse(sc.next());
+        System.out.print("Contract value: ");
+        sc.nextLine();
+        Double totalValue = sc.nextDouble();
+        System.out.print("Enter number of installments: ");
+        Integer months = sc.nextInt();
+
+        System.out.println(months);
+        Contract contract = new Contract(number, date, totalValue);
+        ContractService contractService = new ContractService(new PaypalService());
+        contractService.processContract(contract, months);
+
+        System.out.println("Installments:");
+        for (Installment installment : contract.getInstallments()) {
+            System.out.println(sdf.format(installment.getDueDate()) + " - " + String.format("%.2f", installment.getAmount()));
+        }
+        sc.close();
     }
 }
--- 18.interfaces/exercicio-de-fixacao/src/model/service/ContractService.java
+++ 18.interfaces/exercicio-de-fixacao/src/model/service/ContractService.java
@@ -1,5 +1,34 @@
 package model.service;
 
+import model.entities.Contract;
+import model.entities.Installment;
+
 public class ContractService {
     
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService() {
+
+    }
+    
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public OnlinePaymentService getOnlinePaymentService() {
+        return onlinePaymentService;
+    }
+
+    public void setOnlinePaymentService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, Integer months) {
+        
+        for(Integer i=1; i<= months; i++) {
+            Double amount;
+            amount = onlinePaymentService.interest(contract.getTotalValue()/months, i);
+            contract.getInstallments().add(new Installment(contract.getDate(), amount));
+        }
+    }
 }
