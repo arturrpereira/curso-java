d68d8b9b0374e086098ce9d296cca4032f48de4f
arturrpereira <artur.fernando@protonmail.com>
2022-06-26 23:59:05 -0300

    inclusão da chamada override

--- 18.interfaces/exercicio-de-fixacao/src/model/service/PaypalService.java
+++ 18.interfaces/exercicio-de-fixacao/src/model/service/PaypalService.java
@@ -2,10 +2,12 @@ package model.service;
 
 public class PaypalService implements OnlinePaymentService {
     
+    @Override
     public double paymentFee(Double amount) {
         return amount * 0.02;
     }
 
+    @Override
     public double interest(Double amount, Integer months) {
         amount += amount*((double)months/100);
         amount += paymentFee(amount);
