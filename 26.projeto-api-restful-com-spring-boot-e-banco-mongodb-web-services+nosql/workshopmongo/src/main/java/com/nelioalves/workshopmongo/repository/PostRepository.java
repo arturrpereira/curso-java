10e99cba4c940d52d45c0c02204674fd05377652
arturrpereira <artur.fernando@protonmail.com>
2022-07-20 18:24:32 -0300

    Consulta simples com @Query

--- 26.projeto-api-restful-com-spring-boot-e-banco-mongodb-web-services+nosql/workshopmongo/src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ 26.projeto-api-restful-com-spring-boot-e-banco-mongodb-web-services+nosql/workshopmongo/src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -10,5 +10,8 @@ import com.nelioalves.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+   @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+   List<Post> searchTitle(String text);
+
    List<Post> findByTitleContainingIgnoreCase(String text);
 }
